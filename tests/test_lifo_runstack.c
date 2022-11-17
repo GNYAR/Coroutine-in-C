998bc9736323046b7c13ec795d87c18a0e08029b
GNYAR <s901121s901121@gmail.com>
2022-11-17 17:17:04 +0800

    test(stack): test the functions of runstack

---
+++ tests/test_lifo_runstack.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "src/coroutine_int.h"
+
+int main(void)
+{
+    struct task_struct task[10];
+    struct rs rs;
+    struct task_struct *tmp;
+
+    rs_init(&rs);
+    for (int i = 0; i < 10; i++)
+    {
+        task[i].tfd = i;
+        printf("push %d, return %d\n", i, rs_push(&rs, &task[i]));
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        tmp = rs_pop(&rs);
+        if (tmp)
+            printf("pop %d\n", tmp->tfd);
+        else
+            printf("pop failed\n");
+    }
+
+    return 0;
+}
